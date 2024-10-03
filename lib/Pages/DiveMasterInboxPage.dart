cc44b41da15c6c9b87374d1009758a09e8d25acb
Sarper ERBAR <113534924+sarperbar@users.noreply.github.com>
2024-10-03 21:16:56 +0300

    NFT denemee

--- lib/Pages/DiveMasterInboxPage.dart
+++ lib/Pages/DiveMasterInboxPage.dart
@@ -60,7 +60,7 @@ class _DiveMasterInboxPageState extends State<DiveMasterInboxPage> {
   Future<List<Message>> _fetchMessagesFromBackend(String walletAddress) async {
     final String backendUrl = 'http://192.168.1.3:8080/api/messages/$walletAddress';
     final response = await http.get(Uri.parse(backendUrl));
-    print('Received JSON data: ${response.body}');
+   // print('Received JSON data: ${response.body}');
 
     if (response.statusCode == 200) {
       List<dynamic> data = jsonDecode(response.body);
@@ -71,7 +71,7 @@ class _DiveMasterInboxPageState extends State<DiveMasterInboxPage> {
       }).toList();
 
       List<Message> unconfirmedMessages = allMessages.where((message) => !message.isconfirmed).toList();
-      print('Unconfirmed Messages: ${unconfirmedMessages.join(', ')}');
+
 
       return unconfirmedMessages;
     } else {
@@ -85,19 +85,477 @@ class _DiveMasterInboxPageState extends State<DiveMasterInboxPage> {
         {
           "inputs": [
             {
-              "internalType": "uint256",
-              "name": "num",
-              "type": "uint256"
+              "internalType": "address",
+              "name": "account",
+              "type": "address"
             }
           ],
-          "name": "store",
+          "name": "add",
           "outputs": [],
           "stateMutability": "nonpayable",
           "type": "function"
         },
         {
           "inputs": [],
-          "name": "retrieve",
+          "stateMutability": "nonpayable",
+          "type": "constructor"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "sender",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "balance",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "needed",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "tokenId",
+              "type": "uint256"
+            }
+          ],
+          "name": "ERC1155InsufficientBalance",
+          "type": "error"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "approver",
+              "type": "address"
+            }
+          ],
+          "name": "ERC1155InvalidApprover",
+          "type": "error"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "idsLength",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "valuesLength",
+              "type": "uint256"
+            }
+          ],
+          "name": "ERC1155InvalidArrayLength",
+          "type": "error"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "operator",
+              "type": "address"
+            }
+          ],
+          "name": "ERC1155InvalidOperator",
+          "type": "error"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "receiver",
+              "type": "address"
+            }
+          ],
+          "name": "ERC1155InvalidReceiver",
+          "type": "error"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "sender",
+              "type": "address"
+            }
+          ],
+          "name": "ERC1155InvalidSender",
+          "type": "error"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "operator",
+              "type": "address"
+            },
+            {
+              "internalType": "address",
+              "name": "owner",
+              "type": "address"
+            }
+          ],
+          "name": "ERC1155MissingApprovalForAll",
+          "type": "error"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "account",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "id",
+              "type": "uint256"
+            },
+            {
+              "internalType": "string",
+              "name": "data",
+              "type": "string"
+            }
+          ],
+          "name": "mint",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "to",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256[]",
+              "name": "ids",
+              "type": "uint256[]"
+            },
+            {
+              "internalType": "uint256[]",
+              "name": "amounts",
+              "type": "uint256[]"
+            },
+            {
+              "internalType": "bytes",
+              "name": "data",
+              "type": "bytes"
+            }
+          ],
+          "name": "mintBatch",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "owner",
+              "type": "address"
+            }
+          ],
+          "name": "OwnableInvalidOwner",
+          "type": "error"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "account",
+              "type": "address"
+            }
+          ],
+          "name": "OwnableUnauthorizedAccount",
+          "type": "error"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "account",
+              "type": "address"
+            },
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "operator",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "bool",
+              "name": "approved",
+              "type": "bool"
+            }
+          ],
+          "name": "ApprovalForAll",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "previousOwner",
+              "type": "address"
+            },
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "newOwner",
+              "type": "address"
+            }
+          ],
+          "name": "OwnershipTransferred",
+          "type": "event"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "account",
+              "type": "address"
+            }
+          ],
+          "name": "remove",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "renounceOwnership",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "from",
+              "type": "address"
+            },
+            {
+              "internalType": "address",
+              "name": "to",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256[]",
+              "name": "ids",
+              "type": "uint256[]"
+            },
+            {
+              "internalType": "uint256[]",
+              "name": "values",
+              "type": "uint256[]"
+            },
+            {
+              "internalType": "bytes",
+              "name": "data",
+              "type": "bytes"
+            }
+          ],
+          "name": "safeBatchTransferFrom",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "from",
+              "type": "address"
+            },
+            {
+              "internalType": "address",
+              "name": "to",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "id",
+              "type": "uint256"
+            },
+            {
+              "internalType": "uint256",
+              "name": "value",
+              "type": "uint256"
+            },
+            {
+              "internalType": "bytes",
+              "name": "data",
+              "type": "bytes"
+            }
+          ],
+          "name": "safeTransferFrom",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "operator",
+              "type": "address"
+            },
+            {
+              "internalType": "bool",
+              "name": "approved",
+              "type": "bool"
+            }
+          ],
+          "name": "setApprovalForAll",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "newuri",
+              "type": "string"
+            }
+          ],
+          "name": "setURI",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "operator",
+              "type": "address"
+            },
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "from",
+              "type": "address"
+            },
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "to",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256[]",
+              "name": "ids",
+              "type": "uint256[]"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256[]",
+              "name": "values",
+              "type": "uint256[]"
+            }
+          ],
+          "name": "TransferBatch",
+          "type": "event"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "newOwner",
+              "type": "address"
+            }
+          ],
+          "name": "transferOwnership",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "operator",
+              "type": "address"
+            },
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "from",
+              "type": "address"
+            },
+            {
+              "indexed": true,
+              "internalType": "address",
+              "name": "to",
+              "type": "address"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "id",
+              "type": "uint256"
+            },
+            {
+              "indexed": false,
+              "internalType": "uint256",
+              "name": "value",
+              "type": "uint256"
+            }
+          ],
+          "name": "TransferSingle",
+          "type": "event"
+        },
+        {
+          "anonymous": false,
+          "inputs": [
+            {
+              "indexed": false,
+              "internalType": "string",
+              "name": "value",
+              "type": "string"
+            },
+            {
+              "indexed": true,
+              "internalType": "uint256",
+              "name": "id",
+              "type": "uint256"
+            }
+          ],
+          "name": "URI",
+          "type": "event"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "account",
+              "type": "address"
+            },
+            {
+              "internalType": "uint256",
+              "name": "id",
+              "type": "uint256"
+            }
+          ],
+          "name": "balanceOf",
           "outputs": [
             {
               "internalType": "uint256",
@@ -107,11 +565,110 @@ class _DiveMasterInboxPageState extends State<DiveMasterInboxPage> {
           ],
           "stateMutability": "view",
           "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address[]",
+              "name": "accounts",
+              "type": "address[]"
+            },
+            {
+              "internalType": "uint256[]",
+              "name": "ids",
+              "type": "uint256[]"
+            }
+          ],
+          "name": "balanceOfBatch",
+          "outputs": [
+            {
+              "internalType": "uint256[]",
+              "name": "",
+              "type": "uint256[]"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "account",
+              "type": "address"
+            },
+            {
+              "internalType": "address",
+              "name": "operator",
+              "type": "address"
+            }
+          ],
+          "name": "isApprovedForAll",
+          "outputs": [
+            {
+              "internalType": "bool",
+              "name": "",
+              "type": "bool"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "owner",
+          "outputs": [
+            {
+              "internalType": "address",
+              "name": "",
+              "type": "address"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "bytes4",
+              "name": "interfaceId",
+              "type": "bytes4"
+            }
+          ],
+          "name": "supportsInterface",
+          "outputs": [
+            {
+              "internalType": "bool",
+              "name": "",
+              "type": "bool"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "name": "uri",
+          "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
         }
       ]), // ABI object
-      'Storage',
+      'diveAppContract',
     ),
-    EthereumAddress.fromHex('0x20f9E349b60E3d932B90E466AC4570Cf3dabDB43'),
+    EthereumAddress.fromHex('0x8D332262a250fF0541BB862C52d95d78B6532C03'),
   );
 
 
@@ -124,66 +681,75 @@ class _DiveMasterInboxPageState extends State<DiveMasterInboxPage> {
         content: Text('Wallet address is not available'),
       ));
       return;
+    }else{
+      print(walletAddress);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(walletAddress),
+      ));
     }
 
-    setState(() {
-      _confirmingMessageId = _message.id;
-    });
+
 
     const metamaskUrl = 'metamask://';
     final Uri uri = Uri.parse(metamaskUrl);
     if (await canLaunchUrl(uri)) {
       await launchUrl(uri);
     }
+
+
     print("***bekliyoruzz");
 
     try {
       final result = await _w3mService.requestWriteContract(
         topic: _w3mService.session?.topic ?? ' ',
-        chainId: 'eip155:11155111',
+        chainId: 'eip155:11155111',  // Sepolia
         deployedContract: deployedContract,
-        functionName: 'store',
+        functionName: 'mint',
         transaction: Transaction(
-          from: EthereumAddress.fromHex(walletAddress),
+          from: EthereumAddress.fromHex(_w3mService.session?.address??''),
         ),
-        parameters: [BigInt.from(_message.id)],
-      ).timeout(Duration(seconds: 60), onTimeout: () {
+        parameters: [
+          EthereumAddress.fromHex(_message.sender),
+          BigInt.from(_message.id),
+          _message.location
+        ],
+      ).whenComplete(()=>print("denedik")).timeout(Duration(seconds: 60), onTimeout: () {
         throw TimeoutException('Transaction took too long');
       });
 
-      if (result != null) {
-        print('Transaction Hash: $result');
+      print('NFT mint success: $result');
+
+
+      print("++++++bekledik backend'e yolluyoruuz.");
+
+      final updateUrl = 'http://192.168.1.3:8080/api/messages/${_message.id}/confirm';
+      final response = await http.put(
+        Uri.parse(updateUrl),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'is_confirmed': true}),
+      ).timeout(Duration(seconds: 10));
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Message confirmed on blockchain'),
+        ));
+
+        _fetchMessages();
       } else {
-        print('Transaction result is null');
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('Failed to confirm message'),
+        ));
       }
+
     } catch (error) {
-      print('Error in requestWriteContract: $error');
-    }
+      print('Error minting NFT: $error');
 
-    print("++++++bekledik backend'e yolluyoruuz.");
-
-    final updateUrl = 'http://192.168.1.3:8080/api/messages/${_message.id}/confirm';
-    final response = await http.put(
-      Uri.parse(updateUrl),
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'is_confirmed': true}),
-    ).timeout(Duration(seconds: 10));
-
-    if (response.statusCode == 200) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Message confirmed on blockchain'),
-      ));
-
-      _fetchMessages(); // sayfayı yeniden yüklüyor
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text('Failed to confirm message'),
-      ));
     }
 
     setState(() {
       _confirmingMessageId = null;
     });
+
   }
 
   @override
